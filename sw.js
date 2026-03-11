c50f3ecc19a507e8303604219b1d58cba7e1e805
sonu kumar <37037527+sonu2dp@users.noreply.github.com>
2026-03-11 14:53:07 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+var CACHE = "worktrack-v5";
+self.addEventListener("install", function(e) {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c) {
+    return c.addAll(["index.html","manifest.json","icon-192.png","icon-512.png"]);
+  }));
+});
+self.addEventListener("activate", function(e) {
+  self.clients.claim();
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
+});
+self.addEventListener("fetch", function(e) {
+  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));
+});
