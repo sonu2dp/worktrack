f4e078e1216bbb3ffcc6a0d82bca7c7b3e1c8cbe
SONU KUMAR <37037527+sonu2dp@users.noreply.github.com>
2026-03-25 23:09:26 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE="worktrack-v6";
+var CACHE="wt-pro-v1";
 self.addEventListener("install",function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(["index.html","manifest.json","icon-192.png","icon-512.png"]);}));});
-self.addEventListener("activate",function(e){self.clients.claim();e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}));});
+self.addEventListener("activate",function(e){self.clients.claim();e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(x){return x!==CACHE;}).map(function(x){return caches.delete(x);}));}));});
 self.addEventListener("fetch",function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));});
